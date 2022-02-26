5f2f06f43a05d89d510abf284a3d0679ae707612
Guillaume Delmas-Frenette <guillaume.delmas-frenette@mail.mcgill.ca>
2022-02-26 11:12:36 -0500

    Initial commit

---
+++ app/src/main/java/ca/mcgill/ecse321/carbonfootprintcalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse321.carbonfootprintcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
